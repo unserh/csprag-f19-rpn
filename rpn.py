a8b138d3a88571545be1085a8935bb471385ad91
Unser Hashsham <unserh@umich.edu>
2019-11-01 14:06:44 -0400

    Refactor code to use library functions

--- rpn.py
+++ rpn.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python3
 
-def add(a, b):
-    return a + b
-
-def sub(a, b):
-    return a - b
+import operator
 
 
 operators = {
-    '+' : add,
-    '-' : sub }  
+    '+' : operator.add,
+    '-' : operator.sub }  
 
 def calculate(arg):
 
